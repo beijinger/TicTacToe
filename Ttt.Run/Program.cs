d285df156c1b20b2fda6caa7947b06df6d845bed
fomaa <roman.nofenko@gmail.com>
2016-06-17 15:56:34 -0400

    the first commit

---
+++ Ttt.Run/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ttt.Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new WelcomePage().Show();
+        }
+    }
+}
---
+++ Ttt.Run/WelcomePage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ttt.Run
+{
+    public class WelcomePage
+    {
+        public void Show()
+        {
+            Console.WriteLine("Hello Yan");
+        }
+    }
+}
